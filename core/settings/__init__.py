782ceea6048f954617c68ac36b68c7ea795f2ba0
Onasanya Tunde <Onasanyatunde67@gmail.com>
2021-01-21 20:11:34 +0100

    Added postgres

---
+++ core/settings/__init__.py
@@ -0,0 +1,7 @@
+from .base_settings import *
+from .production_settings import *
+
+try:
+    from .local_settings import *
+except:
+    pass
---
+++ core/settings/production_settings.py
@@ -0,0 +1,15 @@
+import os
+import django_heroku
+DEBUG = os.environ['DEBUG']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+django_heroku.settings(locals())
